1eddf62948d64ae1c250d35ac3c7dc6a3e2ac23a
Risto Ikonen <ristoikonen@gmail.com>
2025-01-26 22:55:41 +1100

    Add project files.

---
+++ Zipper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Zipper;
+
+public static class Zipper
+{
+    public static byte[] Compress(string textToCompress)
+    {
+        var barr = Encoding.ASCII.GetBytes(textToCompress);
+        return QuickLZ.compress(barr, 1);
+    }
+
+    public static string CompressToText(string textToCompress)
+    {
+        var barr = Encoding.ASCII.GetBytes(textToCompress);
+        return Encoding.ASCII.GetString(QuickLZ.compress(barr, 1));
+    }
+
+
+    public static string DeCompress(string compressedText)
+    {
+        var barr = Encoding.ASCII.GetBytes(compressedText);
+        return Encoding.ASCII.GetString(QuickLZ.decompress(barr));
+    }
+
+    public static string DeCompress(byte[] compressedText)
+    {
+        return Encoding.ASCII.GetString(QuickLZ.decompress(compressedText));
+    }
+
+}
